ba1a9d703788e4d575742ca983aa880cb5f5a876
tillw <tillw@Till-PC>
2018-09-28 21:43:29 +0200

    Basic UI

--- JSwing-Paint/src/jsp/Paint.java
+++ JSwing-Paint/src/jsp/Paint.java
@@ -1,5 +1,22 @@
 package jsp;
 
+import java.awt.Graphics;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
+import javax.print.attribute.standard.JobHoldUntil;
+import javax.swing.Box;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.KeyStroke;
+
+import basiX.Dialog;
 import basiX.Fenster;
 import basiX.Maus;
 import basiX.MausLauscherStandard;
@@ -10,15 +27,22 @@ public class Paint implements MausLauscherStandard {
 	private Maus m;
 	private Stift s;
 	private Werkzeugkasten w;
+	private JFrame jf;
 	
 	
 	
 	
 	public Paint() {
-		f = new Fenster("by Till.W", 1280, 720);
+		f = new Fenster("by Till.W", 1000, 800);
 		m = new Maus();
 		f.setzeMausLauscherStandard(this);
 		s = new Stift();
+		jf = f.getMeinJFrame();
+		w = new Werkzeugkasten(s, f);
+		createmenubar();
+		
+		
+		
 	}
 	
 	
@@ -32,8 +56,126 @@ public class Paint implements MausLauscherStandard {
 		
 	}
 
+	private void createmenubar() {
+		final int SHORTCUT_MASK = Toolkit.getDefaultToolkit().getMenuShortcutKeyMask();
+		
+		JMenuBar menubar = new JMenuBar();
+		jf.setJMenuBar(menubar);
+		
+		JMenu menu;
+		JMenuItem item;
 
+		//Datei Menü Anfang
+		
+		menu = new JMenu("Datei");
+		menubar.add(menu);
+		
+		
+		
+		
+		
+		//Zeichenfläche Löschen
+		item = new JMenuItem("Zeichenfläche Leeren");
+		item.setIcon(new ImageIcon(getClass().getResource("icons/canvas.png")));
+		item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N,SHORTCUT_MASK));
+		item.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				if(Dialog.entscheidung("Entscheidung", "Wollen sie die Zeichenfläche leeren?")==true) {
+				f.loescheAlles();
+				}
+			}
+		});
+		menu.add(item);
+		
+		//Bild Speichern Funktion
+		item = new JMenuItem("Speichern",new ImageIcon(getClass().getResource("icons/save.png")));
+		item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S,SHORTCUT_MASK));
+		item.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				f.speichereZeichenflaeche();
+			}
+		});
+		menu.add(item);
+		
+		//Bild Laden Funktion
+		item = new JMenuItem("Laden",new ImageIcon(getClass().getResource("icons/open.png")));
+		item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S,SHORTCUT_MASK));
+		item.addActionListener(new ActionListener() {
+					
+		public void actionPerformed(ActionEvent e) {
+			f.ladeBildInZeichenflaeche();
+			}
+		});
+		menu.add(item);
+		
+		//Datei Menü Ende
 
+		menubar.add(Box.createHorizontalGlue());
+		
+		
+		
+		
+		//
+		
+		JButton ColorButton = new JButton("");
+		ColorButton.setBackground(s.farbe());
+		ColorButton.setOpaque(true);
+		menubar.add(ColorButton);
+		w.cbkennen(ColorButton);
+		
+		//
+		
+		
+		//Ende Knopf Anfang
+		
+		menu = new JMenu("Optionen");
+		menubar.add(menu);
+		
+		
+		
+		item = new JMenuItem("Ende",new ImageIcon(getClass().getResource("icons/exit.png")));
+		item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, SHORTCUT_MASK));
+		item.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(Dialog.entscheidung("Entscheidung", "Wollen sie das Programm wirklich schließen?")==true) {
+					System.exit(0);
+				}
+			}
+		});
+		menu.add(item);
+		
+		
+
+		//Ende Knopf Ende
+		
+		
+		
+		
+		jf.pack();
+		jf.setVisible(true);
+		
+		
+		
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
 
 	public void bearbeiteMausDruck(Object o, int x, int y) {
 		s.runter();
@@ -58,4 +200,6 @@ public class Paint implements MausLauscherStandard {
 	public void bearbeiteMausGezogen(Object o, int x, int y) {
 		
 	}
+	
+	
 }
--- JSwing-Paint/src/jsp/Werkzeugkasten.java
+++ JSwing-Paint/src/jsp/Werkzeugkasten.java
@@ -12,12 +12,9 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.swing.JButton;
+import javax.swing.JColorChooser;
 import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
 import javax.swing.JSlider;
-import javax.swing.KeyStroke;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
@@ -28,46 +25,59 @@ import basiX.Stift;
 
 public class Werkzeugkasten {
     private JFrame fenster;
-   private Fenster malFenster;
+    private Fenster malFenster;
     private Stift stift;
+    private JButton cb;
 
     public Werkzeugkasten(Stift pStift, Fenster pMalFenster) {
         malFenster = pMalFenster;
         stift = pStift;
         fensterErzeugen();
         
+        
 
     }
 
     private void fensterErzeugen() {
-        // TODO Auto-generated method stub
         fenster = new JFrame();
         fenster.setLocation(1000, 0);
-        menuezeileErzeugen(fenster);
         
         JButton knopf = new JButton("BLAU");
         knopf.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
-                // TODO Auto-generated method stub
                 stift.setzeFarbe(Color.BLUE);
                 
             }
         });
         fenster.add(knopf, BorderLayout.EAST);
         
+        
+        
         knopf = new JButton("ROT");
         knopf.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
-                // TODO Auto-generated method stub
                 stift.setzeFarbe(Color.RED);
             }
         });
         
         fenster.add(knopf, BorderLayout.WEST);
+        
+        
+        knopf = new JButton("Farbe");
+        knopf.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Color c = JColorChooser.showDialog(null, null, null);
+				stift.setzeFarbe(c);
+				cb.setBackground(c);
+			}
+		});
+        fenster.add(knopf,BorderLayout.NORTH);
         
         JSlider slider = new JSlider();
         slider.addChangeListener(new ChangeListener() {
@@ -80,85 +90,17 @@ public class Werkzeugkasten {
         });
         fenster.add(slider, BorderLayout.CENTER);
     
+        
+        
+        
         fenster.pack();
         fenster.setVisible(true);
         fenster.setDefaultCloseOperation(fenster.EXIT_ON_CLOSE);
         
     }
     
-    /**
-     * Die Menüzeile des Hauptfensters erzeugen.
-     * 
-     * @param fenster  das Fenster, in das die Menüzeile eingefügt werden soll.
-     */
-    private void menuezeileErzeugen(JFrame fenster)
-    {
-        final int SHORTCUT_MASK =
-            Toolkit.getDefaultToolkit().getMenuShortcutKeyMask();
-
-        JMenuBar menuezeile = new JMenuBar();
-        fenster.setJMenuBar(menuezeile);
-        
-        JMenu menue;
-        JMenuItem eintrag;
-        
-        // Das Datei-Menü erzeugen
-        menue = new JMenu("Datei");
-        menuezeile.add(menue);
-        
-        eintrag = new JMenuItem("Öffnen...");
-            eintrag.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, SHORTCUT_MASK));
-            eintrag.addActionListener(new ActionListener() {
-                               public void actionPerformed(ActionEvent e) { dateiOeffnen(); }
-                           });
-        menue.add(eintrag);
-
-        eintrag = new JMenuItem("Schließen");
-            eintrag.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_W, SHORTCUT_MASK));
-            eintrag.addActionListener(new ActionListener() {
-                               public void actionPerformed(ActionEvent e) { schliessen(); }
-                           });
-        menue.add(eintrag);
-        menue.addSeparator();
-
-        eintrag = new JMenuItem("Speichern");
-            eintrag.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, SHORTCUT_MASK));
-            eintrag.addActionListener(new ActionListener() {
-                               public void actionPerformed(ActionEvent e) { speichern(); }
-                           });
-        menue.add(eintrag);
-        menue.addSeparator();
-        
-        eintrag = new JMenuItem("Beenden");
-            eintrag.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, SHORTCUT_MASK));
-            eintrag.addActionListener(new ActionListener() {
-                               public void actionPerformed(ActionEvent e) { beenden(); }
-                           });
-        menue.add(eintrag);
-
-
-        
-
+    public void cbkennen(JButton pcb) {
+    	cb = pcb;
     }
 
-    protected void beenden() {
-        // TODO Auto-generated method stub
-        
-        System.exit(0);
-    }
-
-    protected void speichern() {
-        // TODO Auto-generated method stub
-        malFenster.speichereZeichenflaeche();
-    }
-
-    protected void schliessen() {
-        // TODO Auto-generated method stub
-        malFenster.loescheAlles();
-    }
-
-    protected void dateiOeffnen() {
-        // TODO Auto-generated method stub
-        malFenster.ladeBildInZeichenflaeche();
-    }
 }
